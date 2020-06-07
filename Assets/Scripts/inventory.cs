0234b6e5b0fe83c1ae376b26d47fd899cd2fed1e
Piri <212310@usn.no>
2020-06-07 12:34:04 +0200

    Added Artefact bonuses

--- Assets/Scripts/inventory.cs
+++ Assets/Scripts/inventory.cs
@@ -12,16 +12,20 @@ public class inventory : MonoBehaviour
 
     private List<Artefact> artefacts;
 
-    public void apply_artefact_bonuses()
+    public Total_artefacts_bonus calculate_artefact_bonuses(string param)
     {
+        // Create a new 
+        Total_artefacts_bonus temp = new Total_artefacts_bonus();
+        temp.reset_bonuses();
+
         foreach (Artefact artefact in artefacts)
         {
-
+            temp.add(artefact.health_bonus, artefact.armor_bonus, artefact.damage_bonus);
         }
 
+        return temp;
 
     }
-
 
 }
 
@@ -31,12 +35,40 @@ class Relic
 
 }
 
+
 class Artefact
 {
+    // Artefacts provide simple bonus to a stat;
+    public int health_bonus = 0;
+    public int armor_bonus = 0;
+    public int damage_bonus = 0;
+}
 
-    private int health_bonus;
-    private int armor_bonus;
-    private int damage_bonus;
-    
+
+public class Total_artefacts_bonus
+{
+    private int hp;
+    private int armor;
+    private int damage;
+
+    // Set all bonuses to zero
+    public void reset_bonuses ()
+    {
+        hp = 0;
+        armor = 0;
+        damage = 0;
+    }
+
+    // Add a value to the total bonus
+    public void add (int hp_bonus, int armor_bonus, int damage_bonus)
+    {
+        hp += hp_bonus;
+        armor += armor_bonus;
+        damage += damage_bonus;
+    }
+
+    public int get_hp() { return hp; }
+    public int get_armor() { return armor; }
+    public int get_damage() { return damage; }
 
 }
